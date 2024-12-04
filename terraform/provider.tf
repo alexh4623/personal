49ccc3c91f5a6fe72692e4291cb6e80ba0b8a005
alexh4623 <alex.hel@yahoo.com>
2024-12-04 22:59:11 +0200

    aws

---
+++ terraform/provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "awssecretsmanager" {}
